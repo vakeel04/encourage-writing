bf4f45118f8391ddb75b6925d2761f399f30e329
a2gpvtltd <a2gpvtltd@gmail.com>
2025-10-27 15:58:50 +0530

    add

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
-const local_url = process.env.LOCAL_URL
+const mongoose = require("mongoose");
+require("dotenv").config();
+const local_url = process.env.LOCAL_URL;
 
-const connect = mongoose.connect(local_url)
-.then(()=>console.log('database is connect success'))
-.catch((err)=>console.log(err,'conection error'))
+const connect = mongoose
+  .connect(local_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database is connect success"))
+  .catch((err) => console.log(err, "conection error"));
 
-module.exports = connect
+module.exports = connect;
---
+++ controllers/dsController/ds_customerPageController.js
@@ -0,0 +1,20 @@
+const Customer = require("../../models/customerModel");
+
+const dsCustomerController = async (req, res) => {
+  try {
+    const data = await Customer.find({});
+    console.log(data);
+
+    res.render("dashboard/ds_manage_customers", {
+      status: true,
+      message: "Customer management page successfully loaded",
+      error: req.query.error || null,
+      data: { customers: data }, // You can later pass actual FAQ data here
+      title: "Manage Customer",
+    });
+  } catch (error) {
+    res.redirect("/error?error=" + error.message);
+  }
+};
+
+module.exports = { dsCustomerController };
